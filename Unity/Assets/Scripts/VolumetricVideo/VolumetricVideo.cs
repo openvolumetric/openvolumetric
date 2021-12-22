e4fce6481a7e4d248c1f335e3708395bef0b8b35
Marco Volino <marco.volino@gmail.com>
2021-12-22 13:21:15 +0000

    Started implementing the unity c# scripts to interface the unity-volumetric-video-api

---
+++ Unity/Assets/Scripts/VolumetricVideo/VolumetricVideo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class VolumetricVideo : MonoBehaviour
+{
+
+    public string video_filename;
+
+    public bool debug = false;
+
+
+    private VolumetricVideoDecoder decoder;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        decoder = new VolumetricVideoDecoder(debug);
+
+        decoder.init(video_filename);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Assets/Scripts/VolumetricVideo/VolumetricVideoDecoder.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Runtime.InteropServices;
+using System;
+
+public class VolumetricVideoDecoder 
+{
+    const string DLLNAME = "NativePlugin";
+    [DllImport(DLLNAME)]
+    private static extern int volumetricvideo_init(ref int ID);
+
+    [DllImport(DLLNAME)]
+    private static extern void volumetricvideo_quit(int ID);
+    
+    [DllImport(DLLNAME)]
+    private static extern void volumetricvideo_open_external_console();
+
+    [DllImport(DLLNAME)]
+    private static extern void volumetricvideo_close_external_console();
+
+
+    int m_instance_id = -1;
+    bool debug;
+
+    public VolumetricVideoDecoder( bool _debug )
+    {
+        Debug.Log("VolumetricVideoDecoder - Constructor");
+
+        debug = _debug;
+
+        if(debug)
+        {
+            Debug.Log("VolumetricVideoDecoder - Opening External Console");
+            volumetricvideo_open_external_console();
+        }
+    }
+
+    ~VolumetricVideoDecoder()
+    {
+        Debug.Log(String.Format("VolumetricVideoDecoder::~VolumetricVideoDecoder - Destructor - id: {0}", m_instance_id));
+        volumetricvideo_quit(m_instance_id);
+
+        Debug.Log(String.Format("VolumetricVideoDecoder::~VolumetricVideoDecoder - Closing External Console - id: {0}", m_instance_id));
+        volumetricvideo_close_external_console();
+    }
+
+
+    public bool init(string video_file)
+    {
+        // Init Instance of Volumetric Video decoder
+        if(volumetricvideo_init(ref m_instance_id) == -1)
+        {
+            Debug.Log("VolumetricVideoDecoder::init - failed to load");
+            return false;
+        }
+        Debug.Log( String.Format("VolumetricVideoDecoder::init - instance ID: {0}",m_instance_id) );
+
+        //
+        return true;
+    }
+
+
+}
