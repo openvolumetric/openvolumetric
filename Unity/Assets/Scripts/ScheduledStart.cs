06482de6a9788baebbe3c784e2db290b04535812
Marco Volino <mv0011@surrey.ac.uk>
2022-02-14 15:49:36 +0000

    Added scheduled start to project

---
+++ Unity/Assets/Scripts/ScheduledStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScheduledStart : MonoBehaviour
+{
+    public double startTimeDSP;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+        double startTime = AudioSettings.dspTime + startTimeDSP; //Start in 10 second
+        Debug.Log(string.Format("DSP Start Time: {0}", startTime));
+
+        var vv_objs = FindObjectsOfType<VolumetricVideo>();
+        foreach (VolumetricVideo vv in vv_objs)
+        {
+            vv.set_scheduled_start(startTime);
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
